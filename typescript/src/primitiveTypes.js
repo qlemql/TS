72096e92d2b4d5a7fca23a22743598be391ad4c0
qlemql <taehyun_fe@naver.com>
2021-09-29 11:24:39 +0900

    typescript 세팅중

--- typescript/src/primitiveTypes.js
+++ typescript/src/primitiveTypes.js
@@ -1,8 +1,15 @@
 var firstName = "kim";
 var age = 50;
+var booolean = true;
 var 널 = null;
 var 언디파인드 = undefined;
 var users = ["kim", "park"];
 var members = { user1: "kin", user2: "park" };
 var people = 123;
-// 타입지정 원래 자동으로 되기에 문법을 생략 가능하다
+// tip 타입지정 원래 자동으로 되기에 문법을 생략 가능하다
+var myFavorite = { singer: "Hyun", song: "blabla" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
